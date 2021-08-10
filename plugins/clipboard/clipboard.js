a730a1de94962589644ff305bb322db055834e44
Yılmaz Oktav <hello@yilmazoktav.com>
2021-08-10 18:17:41 +0300

    Use global toast options in clipboard plugin

--- plugins/clipboard/clipboard.js
+++ plugins/clipboard/clipboard.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueClipboard from 'vue-clipboard2';
 
-import { MOBILE_THRESHOLD_VALUE } from '~/project-constants/breakpoints';
+import { TOAST_OPTIONS } from '~/project-constants/global';
 
 Vue.use(VueClipboard);
 
@@ -13,33 +13,13 @@ export default (content, inject) => {
 export function copyTextWithToast(params) {
   const { text } = params;
 
-  let toastOptions;
-
-  if (document.body.clientWidth < MOBILE_THRESHOLD_VALUE) {
-    toastOptions = {
-      position: 'top',
-      duration: 2000,
-      dismissible: true,
-      queue: true,
-      pauseOnHover: false,
-    };
-  } else {
-    toastOptions = {
-      position: 'top-right',
-      duration: 3000,
-      dismissible: true,
-      queue: false,
-      pauseOnHover: true,
-    };
-  }
-
   // For detail example see: https://www.npmjs.com/package/vue-clipboard2#sample-2
   this.$copyText(text).then(
     () => {
-      this.$toast.success('Successfully copied', toastOptions);
+      this.$toast.success('Successfully copied', TOAST_OPTIONS);
     },
     () => {
-      this.$toast.error('Copy Failed!', toastOptions);
+      this.$toast.error('Copy Failed!', TOAST_OPTIONS);
     },
   );
 }
