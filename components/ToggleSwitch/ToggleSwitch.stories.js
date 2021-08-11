ef86e378d4805d013ee3a922b4e341d983907d8f
Yılmaz Oktav <hello@yilmazoktav.com>
2021-08-11 17:53:03 +0300

    Update ToggleSwitch stories

--- components/ToggleSwitch/ToggleSwitch.stories.js
+++ components/ToggleSwitch/ToggleSwitch.stories.js
@@ -8,8 +8,7 @@ export default {
 export const Template = (arg, { argTypes }) => ({
   props: Object.keys(argTypes),
   template: `<ToggleSwitch
-      :modifier-class="$props.modifierClass"
-      :isActive="$props.isActive"
+      v-model="model"
       :hasText="$props.hasText"
       :primaryText="$props.primaryText"
       :secondaryText="$props.secondaryText"
