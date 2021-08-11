25607c43f3e3dcf3bc7a2aa2f3ea1ccce68432f8
Yılmaz Oktav <hello@yilmazoktav.com>
2021-08-11 13:56:14 +0300

    Add slots to Modal component

--- components/Modal/Modal.vue
+++ components/Modal/Modal.vue
@@ -1,17 +1,35 @@
 <template>
   <transition name="modal">
-    <div v-if="modalState" class="c-modal">
-      <div class="c-modal__background" @click="$emit('closeModal')"></div>
+    <div v-if="value" class="c-modal">
+      <div class="c-modal__background" @click="closeModal"></div>
 
       <div class="c-modal__container">
-        <button type="button" class="c-modal__close" @click="$emit('closeModal')">
-          <Tooltip content="Close" :hide-on-mobile="true">
+        <button type="button" class="c-modal__close" @click="closeModal">
+          <Tooltip content="Kapat" :hide-on-mobile="true">
             <svg-icon name="IconCross" class="u-cursor-pointer" />
           </Tooltip>
         </button>
 
-        <div class="c-modal__content">
-          <slot />
+        <div class="c-modal__content-wrapper">
+          <h3 class="c-modal__title">
+            <slot name="header" />
+          </h3>
+
+          <p class="c-modal__description">
+            <slot name="description" />
+          </p>
+
+          <div class="c-modal__content">
+            <slot name="content" />
+          </div>
+
+          <div class="u-margin-top-large u-display-flex u-justify-content-center">
+            <slot name="footer">
+              <Button theme="primary" type="button" tag="button" @click.native="closeModal">
+                Close
+              </Button>
+            </slot>
+          </div>
         </div>
       </div>
     </div>
@@ -21,9 +39,15 @@
 <script>
 export default {
   props: {
-    modalState: {
+    value: {
       type: Boolean,
-      default: null,
+      default: false,
+    },
+  },
+
+  methods: {
+    closeModal() {
+      this.$emit('input', false);
     },
   },
 };
