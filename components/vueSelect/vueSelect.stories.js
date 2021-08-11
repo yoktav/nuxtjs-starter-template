6f88d1a94015e9240df3a68db7d35682f334823a
Yılmaz Oktav <hello@yilmazoktav.com>
2021-08-11 14:06:00 +0300

    Update vueSelect stories

--- components/vueSelect/vueSelect.stories.js
+++ components/vueSelect/vueSelect.stories.js
@@ -12,7 +12,11 @@ export const Template = (arg, { argTypes }) => ({
   props: Object.keys(argTypes),
   template: `<div>
     For more options see: <a target="_blank" href="https://vue-select.org/guide/options.html#option-labels">https://vue-select.org/guide/options.html#option-labels</a>
+
     <br />
-    <v-select :options="$props.options"></v-select>
+
+    <v-select :options="$props.options">
+      <div slot="no-options">Can't find any results.</div>
+    </v-select>
   </div>`,
 });
