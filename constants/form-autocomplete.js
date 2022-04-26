3bbb77c5488f27a40fbc7d4a75b9f82bbb3afa92
Yılmaz Oktav <hello@yilmazoktav.com>
2022-04-26 17:53:14 +0300

    Create form-autocomplete constants

---
+++ constants/form-autocomplete.js
@@ -0,0 +1,9 @@
+export const FORM_AUTOCOMPLETE = {
+  FAMILY_NAME: 'family-name',
+  GIVEN_NAME: 'given-name',
+  EMAIL: 'email',
+  NEW_PASSWORD: 'new-password',
+  CURRENT_PASSWORD: 'current-password',
+  TEL: 'tel',
+  ONE_TIME_CODE: 'one-time-code',
+};
