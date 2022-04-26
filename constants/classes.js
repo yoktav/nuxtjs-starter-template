377b497b8f8a5573f871f003e4a642b4f3eef658
Yılmaz Oktav <hello@yilmazoktav.com>
2022-04-26 15:13:02 +0300

    Create classes constant

---
+++ constants/classes.js
@@ -0,0 +1,10 @@
+export const CLASS_NAME_IS_ACTIVE = 'is-active';
+export const CLASS_NAME_IS_OPEN = 'is-open';
+export const CLASS_NAME_IS_HIDDEN = 'is-hidden';
+export const CLASS_NAME_IS_SMALL = 'is-small';
+export const CLASS_NAME_IS_DISABLED = 'is-disabled';
+
+export const CLASS_NAME_DISPLAY_NONE = 'u-display-none';
+
+export const CLASS_NAME_ROW = 'row';
+export const CLASS_NAME_COL = 'col';
