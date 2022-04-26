12326c17ef8480de5c0e08c77dca942e5854f765
Yılmaz Oktav <hello@yilmazoktav.com>
2022-04-26 18:01:22 +0300

    Create html tags constant

---
+++ constants/html-tags.js
@@ -0,0 +1,14 @@
+export const TAG_BUTTON = 'button';
+export const TAG_A = 'a';
+export const TAG_DIV = 'div';
+export const TAG_SMALL = 'small';
+export const TAG_IMG = 'img';
+
+export const TAG_H1 = 'h1';
+export const TAG_H2 = 'h2';
+export const TAG_H3 = 'h3';
+export const TAG_H4 = 'h4';
+export const TAG_H5 = 'h5';
+export const TAG_H6 = 'h6';
+
+export const TAG_HR = 'hr';
