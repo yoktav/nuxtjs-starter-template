220bef715a20e4e28134d7cd350fd04b6e8e286b
Yılmaz Oktav <hello@yilmazoktav.com>
2022-04-26 15:24:37 +0300

    Create events constant

---
+++ constants/events.js
@@ -0,0 +1,11 @@
+//
+// Inherited from Bootstrap Vue
+// https://github.com/bootstrap-vue/bootstrap-vue
+//
+
+export const EVENT_NAME_CLICK = 'click';
+export const EVENT_NAME_INPUT = 'input';
+export const EVENT_NAME_TOGGLE = 'toggle';
+
+export const EVENT_OPTIONS_PASSIVE = { passive: true };
+export const EVENT_OPTIONS_NO_CAPTURE = { passive: true, capture: false };
