4d9442e10fac55eb767f659458fe5a1a05c6fdd5
Yılmaz Oktav <hello@yilmazoktav.com>
2022-04-26 15:07:11 +0300

    Add breadcrumb to account page

--- pages/account.vue
+++ pages/account.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container u-margin-top-large">
+    <Breadcrumb class="u-margin-bottom" :links="BREADCRUMBS.ACCOUNT" />
+
     <div class="row">
       <div class="col col--xs-12 u-text-align-left">
-        <h1 class="u-font-weight-600 u-margin-bottom-medium">
-          Welcome to <b class="u-color-primary">Account Page !</b>
-        </h1>
+        <h1 class="u-font-weight-600 u-margin-bottom-medium">Welcome to <b class="u-color-primary">Account Page !</b></h1>
 
         <Button theme="primary" tag="NuxtLink" :to="ROUTE_NAMES.HOME.PATH">Go to home </Button>
       </div>
@@ -13,7 +13,15 @@
 </template>
 
 <script>
+import { BREADCRUMBS } from '~/constants/routes';
+
 export default {
+  data() {
+    return {
+      BREADCRUMBS,
+    };
+  },
+
   mounted() {
     this.$nextTick(() => {
       this.$nuxt.$loading.finish();
