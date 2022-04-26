a192bd1a9f1d83c3bd3d9db1c465bfc5ccbbec98
Yılmaz Oktav <hello@yilmazoktav.com>
2022-04-26 15:08:27 +0300

    Add breadcrumb to example page

--- pages/page.vue
+++ pages/page.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container u-margin-top-large">
+    <Breadcrumb class="u-margin-bottom" :links="BREADCRUMBS.EXAMPLE_PAGE" />
+
     <div class="row">
       <div class="col col--xs-12 u-text-align-left">
-        <h1 class="u-font-weight-600 u-margin-bottom-medium">
-          Welcome to <b class="u-color-primary">Page !</b>
-        </h1>
+        <h1 class="u-font-weight-600 u-margin-bottom-medium">Welcome to <b class="u-color-primary">Page !</b></h1>
 
         <Button theme="primary" tag="NuxtLink" :to="ROUTE_NAMES.HOME.PATH">Go to home </Button>
       </div>
@@ -13,8 +13,16 @@
 </template>
 
 <script>
+import { BREADCRUMBS } from '~/constants/routes';
+
 export default {
   layout: 'full',
+
+  data() {
+    return {
+      BREADCRUMBS,
+    };
+  },
 
   mounted() {
     this.$nextTick(() => {
