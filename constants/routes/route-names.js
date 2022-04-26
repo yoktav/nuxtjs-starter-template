acdbabf7bd696712deb3dc443487c1210f8f760c
Yılmaz Oktav <hello@yilmazoktav.com>
2022-04-26 15:01:22 +0300

    Add page title and meta variables to route names

--- constants/routes/route-names.js
+++ constants/routes/route-names.js
@@ -6,15 +6,27 @@ export const ROUTE_NAMES = {
   HOME: {
     NAME: 'index',
     PATH: '/',
+    PAGE_TITLE: 'Homepage',
+    META: {
+      title: 'Homepage',
+    },
   },
 
   PAGE: {
     NAME: 'page',
     PATH: '/page',
+    PAGE_TITLE: 'Example Page',
+    META: {
+      title: 'Example Page',
+    },
   },
 
   ACCOUNT: {
     NAME: 'account',
     PATH: '/account',
+    PAGE_TITLE: 'Account Page',
+    META: {
+      title: 'Account Page',
+    },
   },
 };
