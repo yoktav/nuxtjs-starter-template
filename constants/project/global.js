3de93171561ca488e8969d155236d97db7c8b81d
Yılmaz Oktav <hello@yilmazoktav.com>
2022-04-27 10:37:29 +0300

    Add date constants to global constant file

--- constants/project/global.js
+++ constants/project/global.js
@@ -1,1 +1,9 @@
 export const LOADING_SCREEN_MIN_DURATION = 1700;
+
+//
+// Date
+//
+
+export const ONE_DAY_AS_MS = 86400000;
+export const ONE_DAY_AS_SECOND = 86400;
+export const ONE_MINUTE_AS_MS = 60000;
